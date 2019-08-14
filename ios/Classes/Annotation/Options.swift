b08499d2c05ededcaa9fba27f63aec288524c7c9
linsikai123 <linsikai@ifreegroup.com>
2019-08-14 11:03:22 +0800

    去除断言

--- ios/Classes/Annotation/Options.swift
+++ ios/Classes/Annotation/Options.swift
@@ -3,6 +3,7 @@ class Options<G: Geometry> {
 
     // Work around because Swift does not support generic protocols (or abstract classes).
     func build(id: UInt64) -> Feature<G>? {
-        assert(false, "This method must be overriden by the subclass")
+//        assert(false, "This method must be overriden by the subclass")
+        return nil
     }
 }
